ce04d8b84ed019d9d65bc1f667ed5e1e2e0d7dd9
Delaney <112586752+deedavis06@users.noreply.github.com>
2022-10-04 12:08:46 -0500

    Add files via upload
    Pricing for the Ft Worth Zoo

---
+++ Zoo Prices.py
@@ -0,0 +1,14 @@
+age = 65
+if (age >= 3) :
+    print("You can go to the zoo")
+    if (age <= 12) :
+        print('Ticket price is 13$')
+    if (13 <= age <= 64) :
+        print ('Ticket price is 17$')
+    if (age >= 65) :
+        print ("Ticket price is 13$")
+else :
+    print('You are not eligible for a ticket')
+
+
+
