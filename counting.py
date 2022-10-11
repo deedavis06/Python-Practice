40f739096c714d68b2d1db05eee63683b6d8a37e
Delaney <112586752+deedavis06@users.noreply.github.com>
2022-10-11 15:07:52 -0500

    Update counting.py

--- counting.py
+++ counting.py
@@ -1,14 +1,17 @@
+#user inputs their favorite movie quote
 counts = dict()
-print('Enter your favorite famous line from a movie')
+print('Enter your favorite movie quote')
 line = input('')
 
 words = line.split()
 print('Words:', words)
 
+#uses a dictionary to store and count freqency of words 
 for w in words:
     counts[w] = counts.get(w,0)+1
 print('Counts', counts)
 
+#what word has most frequently occured  in the quote and how many times did it occur?
 bigcount = None
 bigword = None
 for w,c in counts.items() :
