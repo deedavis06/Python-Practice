634156b88cdaef1ebcd53d700aec97a82e714f18
Delaney <112586752+deedavis06@users.noreply.github.com>
2022-10-13 15:42:45 -0500

    Update regular_expressions.py

--- regular_expressions.py
+++ regular_expressions.py
@@ -1,14 +1,14 @@
 import re 
 em = 'From: ddd6@unt.edx.edu Thurs Oct 13 14:58:15 2022'
    
-    #extract the email address from string
+#extract the email address from string
 ad = re.findall('\S+@\S+', em)
 print(ad)
 
-    #extract the domain from the email address
+#extract the domain from the email address
 ad2 = re.findall('@[^ ]*', em)
 print(ad2)
 
-    #extract the domain without the @
+#extract the domain without the @
 ad3 = re.findall('^From: .*@([^ ]*)', em)
 print(ad3)
