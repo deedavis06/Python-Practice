5fe905dee576886678309ccdd4fa360fa52982c9
Delaney <112586752+deedavis06@users.noreply.github.com>
2022-10-15 13:30:15 -0500

    Update XML Practice.py

--- XML Practice.py
+++ XML Practice.py
@@ -3,12 +3,9 @@ import xml.etree.ElementTree as ET
 data = ET.Element('Person')
 
 element1 = ET.SubElement(data, 'Email')
-data.append(element1)
 
-sel_1 = ET.SubElement(element1, 'Employee Address')
-sel_1.text = 'ddd@unt.ex.edu'
-sel_2 = ET.SubElement(element1, 'Student Address')
-sel_2.text = 'ddd1@my.unt.ex.edu'
+ET.Subelement(element1, 'Field 1', name = 'Employee Address').text = 'ddd@unt.ex.edu'
+ET.Subelement(element1, 'Field 2', name = 'Student Address').text = 'ddd1@my.unt.ex.edu'
 
-tree = ET.ElementTree(data)
-print(tree)
+tre = ET.ElementTree(data)
+tre.write('XMLPractice.xml')
